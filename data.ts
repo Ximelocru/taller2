fa5a75213f3f0f066caba32a39e469a82bcd9ad7
ximelocru <ax.lopez@uniandes.edu.co>
2024-10-20 23:43:40 -0500

    commit

--- data.ts
+++ data.ts
@@ -1,21 +1,21 @@
 import {Serie} from './Serie.js';
 export const series = [
     new Serie (1,"Breaking Bad","AMC", 5,"Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer" ,
-    "https://www.amc.com/shows/breaking-bad","https://i.imgur.com/GGje0vc.jpg") ,
+    "https://www.amc.com/shows/breaking-bad","https://m.media-amazon.com/images/M/MV5BMTc5NzE3NTc0MV5BMl5BanBnXkFtZTcwMDk5MDIzMw@@._V1_.jpg") ,
     
     new Serie (2,"Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary", 
-    "https://www.netflix.com/co/title/70242311","https://i.imgur.com/EvKe48G.jpg"),
+    "https://www.netflix.com/co/title/70242311","https://lionsgate.brightspotcdn.com/54/44/2607cd4c422ca8b73b6a0b7d4c20/2740007-z-orank2099cau1.jpg"),
     
     new Serie (3, "Game of Thrones","HBO", 7, "American fantasy drama", "https://www.hbo.com/game-of-thrones",
-      "https://i.imgur.com/TDCEV1S.jpg"),
+      "https://hdqwalls.com/wallpapers/daenerys-targaryen-game-of-thrones-season-7-fp.jpg"),
 
     new Serie (4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life. Despite their on-again, off-again relationship in the past, Leonard and Penny have finally gotten married. Even Sheldon has found a female companion, entering into a relationship agreement with neurobiologist Amy Farrah Fowler, and he recently took their relationship to the next level by marrying her after a long courtship. In their free time, Leonard and Sheldon enjoy fantasy role-playing games with their ever-expanding universe of friends, including fellow scientists Koothrappali, Wolowitz, and Wolowitz’s adorable microbiologist wife, Bernadette, who is adjusting to life with their two children.",
-        "https://www.cbs.com/shows/big_bang_theory/about/", "https://i.imgur.com/uAEpVWk.jpg"),
+        "https://www.cbs.com/shows/big_bang_theory/about/", "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/27dad4f1f79ea44b96fc726ddef21d04294c1b1654e06d6126c256f4db76054c._RI_V_TTW_.jpg"),
     
     new Serie (5, "Sherlock", "BBC",  4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps",
-        "https://www.bbc.co.uk/programmes/b018ttws", "https://i.imgur.com/02B7qhj.jpg"),
+        "https://www.bbc.co.uk/programmes/b018ttws", "https://media.senscritique.com/media/000006170866/source_big/Sherlock.jpg"),
 
     new Serie (6, "A Very English Scandal", "BBC", 2, "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.",
-        "https://www.bbc.co.uk/programmes/p065smy4", "https://i.imgur.com/D4y3DrQ.jpg"),
+        "https://www.bbc.co.uk/programmes/p065smy4", "https://cultbox.co.uk/wp-content/uploads/2018/05/MV5BMjliY2I4MDUtOGM4Ny00YjJhLWJlNmMtMjE3YmQ0ZmNhMzhmXkEyXkFqcGdeQXVyNTUwNDQ4NQ@@._V1_SY1000_CR0013331000_AL_.jpg"),
   ];
    
